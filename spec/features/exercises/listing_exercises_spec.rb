f72c7b13258c2f403d5c70ed58ae31a2b746dad8
israelhz <israel.leal25@gmail.com>
2018-08-10 15:11:01 -0500

    Adds feature specs for listing workouts

---
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature 'Listing Exercises' do
+  before do
+    @john = User.create(email: 'john@example.com', password: 'password')
+    login_as @john
+
+    @exercise1 = @john.exercises.create(duration_in_min: 20,
+                                        workout: 'My body building activity',
+                                        workout_date: Date.today)
+    @exercise2 = @john.exercises.create(duration_in_min: 35,
+                                        workout: 'Weight lifting',
+                                        workout_date: 2.days.ago)
+  
+    fred = User.create(email: 'fred@example.com', password: 'password')
+    @exercise3 = fred.exercises.create(duration_in_min: 40,
+                                       workout: 'Push upds',
+                                       workout_date: 1.day.ago)
+  end
+
+  scenario "shows user's workout for last 7 days" do
+    visit '/'
+
+    click_link 'My Lounge'
+
+    expect(page).to have_content(@exercise1.duration_in_min)
+    expect(page).to have_content(@exercise1.workout)
+    expect(page).to have_content(@exercise1.workout_date)
+
+    expect(page).to have_content(@exercise2.duration_in_min)
+    expect(page).to have_content(@exercise2.workout)
+    expect(page).to have_content(@exercise2.workout_date)
+
+    expect(page).not_to have_content(@exercise3.duration_in_min)
+    expect(page).not_to have_content(@exercise3.workout)
+    expect(page).not_to have_content(@exercise3.workout_date)
+   end
+end
