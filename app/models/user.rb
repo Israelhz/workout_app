8047bfeda0120dc065b73b9cb96b7aa88ef65d0e
israelhz <israel.leal25@gmail.com>
2018-08-10 21:41:12 -0500

    Removes foreign key from friend

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :exercises
 
   has_many :friendships
-  has_many :friends, through: :friendships, class_name: 'User'
+  has_many :friends, through: :friendships, class_name: 'User', foreign_key: "friend_id"
 
   validates :first_name, :last_name, presence: true
 
